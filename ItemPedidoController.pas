27fd7a4bf9692a37ae5bb6b09ee68a1dce3bfd56
WandersonMikchael <102076177+WandersonMikchael@users.noreply.github.com>
2024-10-14 11:08:11 -0300

    Add files via upload

---
+++ ItemPedidoController.pas
@@ -0,0 +1,45 @@
+unit ItemPedidoController;
+
+interface
+
+uses
+  ItemPedidoModel, ItemPedidoRepository;
+
+type
+  TItemPedidoController = class
+  private
+    FItemPedidoRepo: TItemPedidoRepository;
+  public
+    constructor Create(AItemPedidoRepo: TItemPedidoRepository);
+
+    procedure AdicionarItem(NumeroPedido: Integer; AItem: TItemPedido);
+    procedure AtualizarItem(NumeroPedido: Integer; AItem: TItemPedido);
+    procedure DeletarItensPorPedido(NumeroPedido: Integer);
+  end;
+
+implementation
+
+{ TItemPedidoController }
+
+constructor TItemPedidoController.Create(AItemPedidoRepo: TItemPedidoRepository);
+begin
+  FItemPedidoRepo := AItemPedidoRepo;
+end;
+
+procedure TItemPedidoController.AdicionarItem(NumeroPedido: Integer; AItem: TItemPedido);
+begin
+  FItemPedidoRepo.InserirItem(NumeroPedido, AItem.CodigoProduto, AItem.Quantidade, AItem.ValorUnitario, AItem.ValorTotal);
+end;
+
+procedure TItemPedidoController.AtualizarItem(NumeroPedido: Integer; AItem: TItemPedido);
+begin
+  FItemPedidoRepo.AtualizarItem(NumeroPedido, AItem.CodigoProduto, AItem.Quantidade, AItem.ValorUnitario, AItem.ValorTotal);
+end;
+
+procedure TItemPedidoController.DeletarItensPorPedido(NumeroPedido: Integer);
+begin
+  FItemPedidoRepo.DeletarItensPorPedido(NumeroPedido);
+end;
+
+end.
+
---
+++ PedidoController.pas
@@ -0,0 +1,120 @@
+unit PedidoController;
+
+interface
+
+uses
+  PedidoModel, ItemPedidoModel, PedidoRepository, ItemPedidoRepository,
+  System.SysUtils, System.Generics.Collections, Data.DB, FireDAC.Comp.Client;
+
+type
+  TPedidoController = class
+  private
+    FPedidoRepo: TPedidoRepository;
+    FItemPedidoRepo: TItemPedidoRepository;
+
+    function CalcularValorTotal(AItens: TObjectList<TItemPedido>): Double;
+
+  public
+    constructor Create(APedidoRepo: TPedidoRepository; AItemPedidoRepo: TItemPedidoRepository);
+
+    function CriarPedido(APedido: TPedido): Integer;
+    procedure AtualizarPedido(APedido: TPedido);
+    procedure DeletarPedido(NumeroPedido: Integer);
+    function CarregarPedido(NumeroPedido: Integer): TPedido;
+  end;
+
+implementation
+
+{ TPedidoController }
+
+constructor TPedidoController.Create(APedidoRepo: TPedidoRepository; AItemPedidoRepo: TItemPedidoRepository);
+begin
+  FPedidoRepo := APedidoRepo;
+  FItemPedidoRepo := AItemPedidoRepo;
+end;
+
+function TPedidoController.CalcularValorTotal(AItens: TObjectList<TItemPedido>): Double;
+var
+  Item: TItemPedido;
+begin
+  Result := 0;
+  for Item in AItens do
+    Result := Result + Item.ValorTotal;
+end;
+
+function TPedidoController.CriarPedido(APedido: TPedido): Integer;
+var
+  NumeroPedido: Integer;
+  Item: TItemPedido;
+begin
+  APedido.AtualizarValorTotal;
+
+  NumeroPedido := FPedidoRepo.InserirPedido(APedido.CodigoCliente, APedido.ValorTotal);
+
+  for Item in APedido.Itens do
+  begin
+    FItemPedidoRepo.InserirItem(NumeroPedido, Item.CodigoProduto, Item.Quantidade, Item.ValorUnitario, Item.ValorTotal);
+  end;
+
+  Result := NumeroPedido;
+end;
+
+procedure TPedidoController.AtualizarPedido(APedido: TPedido);
+var
+  Item: TItemPedido;
+begin
+  APedido.AtualizarValorTotal;
+
+  FPedidoRepo.AtualizarPedido(APedido.NumeroPedido, APedido.CodigoCliente, APedido.ValorTotal);
+
+  for Item in APedido.Itens do
+  begin
+    FItemPedidoRepo.AtualizarItem(APedido.NumeroPedido, Item.CodigoProduto, Item.Quantidade, Item.ValorUnitario, Item.ValorTotal);
+  end;
+end;
+
+procedure TPedidoController.DeletarPedido(NumeroPedido: Integer);
+begin
+  FItemPedidoRepo.DeletarItensPorPedido(NumeroPedido);
+  FPedidoRepo.DeletarPedido(NumeroPedido);
+end;
+
+function TPedidoController.CarregarPedido(NumeroPedido: Integer): TPedido;
+var
+  Pedido: TPedido;
+  Item: TItemPedido;
+  QryPedido, QryItens: TFDQuery;
+begin
+  QryPedido := FPedidoRepo.ObterPedido(NumeroPedido);
+  try
+    if not QryPedido.IsEmpty then
+    begin
+      Pedido := TPedido.Create;
+      Pedido.NumeroPedido := NumeroPedido;
+      Pedido.CodigoCliente := QryPedido.FieldByName('CodigoCliente').AsInteger;
+      Pedido.ValorTotal := QryPedido.FieldByName('ValorTotal').AsFloat;
+      Pedido.DataEmissao := QryPedido.FieldByName('DataEmissao').AsDateTime;
+
+      QryItens := FItemPedidoRepo.ObterItensPorPedido(NumeroPedido);
+      try
+        while not QryItens.Eof do
+        begin
+          Item := TItemPedido.Create(QryItens.FieldByName('CodigoProduto').AsInteger, QryItens.FieldByName('Descricao').AsString,
+            QryItens.FieldByName('Quantidade').AsInteger, QryItens.FieldByName('ValorUnitario').AsFloat);
+          Pedido.AdicionarItem(Item);
+          QryItens.Next;
+        end;
+      finally
+        QryItens.Free;
+      end;
+    end
+    else
+      raise Exception.Create('Pedido não encontrado.');
+  finally
+    QryPedido.Free;
+  end;
+  Result := Pedido;
+end;
+
+end.
+
